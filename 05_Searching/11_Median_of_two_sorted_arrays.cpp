c42c4430452f10bb8ec48c0291c2c4eab9eb7f17
Shruti Sureshan <shrutisureshan@gmail.com>
2022-01-01 12:51:52 +0530

    Update 11_Median_of_two_sorted_arrays.cpp

--- 05_Searching/11_Median_of_two_sorted_arrays.cpp
+++ 05_Searching/11_Median_of_two_sorted_arrays.cpp
@@ -4,7 +4,7 @@
 
 //Naive sol: put elements of ai and a2 in a new array temp[] and then sort it. Time=O((n1+n2)*log(n1+n2))
 
-//Better apporach using Binary search:
+//Better apporach using Binary search: Time=O(log n1) where n1<=n2
 
 #include <iostream>
 #include <cmath>
@@ -23,12 +23,19 @@ double getMed(int a1[], int a2[], int n1, int n2)
 	{
 		int i1 = (begin1 + end1) / 2;
 		int i2 = (n1 + n2 + 1) / 2 - i1;
-
-		int min1 = (i1 == n1)?INT_MAX:a1[i1];
-		int max1 = (i1 == 0)?INT_MIN:a1[i1 - 1];
 		
-		int min2 = (i2 == n2)?INT_MAX:a2[i2];
-		int max2 = (i2 == 0)?INT_MIN:a2[i2 - 1];
+		/*
+		min1=Min element in RHS of a1
+		max1=Max elelement in LHS of a1
+		min2=Min element in RHS of a2
+		max2=Max elelement in LHS of a2
+		*/
+		
+		int min1 = (i1 == n1)?INT_MAX:a1[i1];	//check if there is nothing on RHS        
+		int max1 = (i1 == 0)?INT_MIN:a1[i1 - 1];      //check if there is nothing on LHS  
+		
+		int min2 = (i2 == n2)?INT_MAX:a2[i2];		
+		int max2 = (i2 == 0)?INT_MIN:a2[i2 - 1];        
 
 		if(max1 <= min2 && max2 <= min1)
 		{
