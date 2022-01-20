783821ef89d21c45e61a0b8cbbef303a6e740a60
Shruti Sureshan <shrutisureshan@gmail.com>
2022-01-20 23:49:19 +0530

    Create Problem10_Swap_all_odd_and_even_bits.cpp

---
+++ 02_Bit Magic/Problem10_Swap_all_odd_and_even_bits.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to swap odd and even bits.
+    unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	unsigned int e=n & 0xAAAAAAAA;
+    	unsigned int odd=n & 0x55555555;
+    	e>>=1;
+    	odd<<=1;
+    	
+    	return (e|odd);
+    }
+};
+
+// { Driver Code Starts.
+
+// Driver code
+int main()
+{
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		unsigned int n;
+		cin>>n;//input n
+		
+		Solution ob;
+		//calling swapBits() method
+		cout << ob.swapBits(n) << endl;
+	}
+	return 0;
+}  // } Driver Code Ends
