c2f59fac33ac7433fc714923971ce2cafe7e3452
Shruti Sureshan <shrutisureshan@gmail.com>
2022-01-31 18:46:39 +0530

    Create Problem10_Maximum_index.cpp

---
+++ 04_Arrays/Problem10_Maximum_index.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int A[], int N) 
+    { 
+        // Your code here
+        int mx=0,i=0,j=N-1;
+        while(i <=j){
+        if(A[i]<=A[j]){
+            mx=max(mx,j-i);
+            i++;
+            j=N-1;
+        }
+        else{
+            j--;
+        }
+}
+
+return mx;
+    }
+};
+
+// { Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+}   // } Driver Code Ends
